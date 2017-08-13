a96f2e281c6fad78eba0f8f5db01b1b4077191a4
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2017-08-13 19:24:01 +1000

    Correct react import

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as React from 'React';
+import * as React from 'react';
 import injectToPropTypes from './injectToPropTypes';
 import runTest from './runTest';
 import * as _ from 'lodash';
--- src/runTest.tsx
+++ src/runTest.tsx
@@ -1,4 +1,4 @@
-import * as React from 'React';
+import * as React from 'react';
 import * as ReactTestRenderer from 'react-test-renderer';
 
 const runTest = (
